def6873fdffcf3d92e3fdd53d1d42eb8d370258d
zn-rabby <rabby.webdeveloper@gmail.com>
2025-06-16 15:48:03 +0600

    feat(auth): implement OTP resend functionality via email with 3-minute expiration

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -250,31 +250,21 @@ const changePasswordToDB = async (
 
 // resend otp
 const resendOtpFromDb = async (email: string) => {
-  // Check if the user exists
-  const isExistUser = await User.isExistUserByEmail(email);
-  if (!isExistUser || !isExistUser._id) {
-    throw new ApiError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
-  }
+     // Check if the user exists
+     const isExistUser = await User.isExistUserByEmail(email);
+     if (!isExistUser || !isExistUser._id) {
+          throw new ApiError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
+     }
 
-  // send email
-  const otp = generateOTP(4);
-  const values = {
-    name: isExistUser.name,
-    otp: otp,
-    email: isExistUser.email!,
-  };
-  const createAccountTemplate = emailTemplate.createAccount(values);
-  emailHelper.sendEmail(createAccountTemplate);
+     // send email
+     const otp = generateOTP(4);
+     const values = { name: isExistUser.name, otp: otp, email: isExistUser.email! };
+     const createAccountTemplate = emailTemplate.createAccount(values);
+     emailHelper.sendEmail(createAccountTemplate);
 
-  //save to DB
-  const authentication = {
-    oneTimeCode: otp,
-    expireAt: new Date(Date.now() + 3 * 60000),
-  };
-  await User.findOneAndUpdate(
-    { _id: isExistUser._id },
-    { $set: { authentication } }
-  );
+     //save to DB
+     const authentication = { oneTimeCode: otp, expireAt: new Date(Date.now() + 3 * 60000) };
+     await User.findOneAndUpdate({ _id: isExistUser._id }, { $set: { authentication } });
 };
 
 export const AuthService = {
